87e364d1ee725e1618e8331fd9ec25aeacb5ef83
batsheva <batsheva4050@gmail.com>
2023-11-29 12:16:44 +0200

    fix several things

--- public/js_countries/countryClass.js
+++ public/js_countries/countryClass.js
@@ -30,7 +30,7 @@ export default class CountryClass{
   }
 
   async delCountry(){
-    let url = "http://localhost:3001/countries/"+this.id;
+    let url = "https://first-try-2jm0.onrender.com/countries/"+this.id;
     try{
 
       let resp =  await axios({
